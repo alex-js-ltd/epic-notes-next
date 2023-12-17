19c10fa30e1416ca7fff1fd835aa1bacf3cae2f3
Alexander Gilbert <alexandergilbert@Alexanders-MacBook-Pro.local>
2023-12-17 13:34:02 +0000

    update custom link

--- app/comps/custom-link.tsx
+++ app/comps/custom-link.tsx
@@ -12,16 +12,10 @@ interface CustomLinkProps extends NextLinkProps {
 export default function CustomLink({ className, ...props }: CustomLinkProps) {
 	const pathname = usePathname()
 
-	const navLinkDefaultClassName =
-		'line-clamp-2 block rounded-l-full py-2 pl-8 pr-6 text-base lg:text-xl'
-
 	return (
 		<NextLink
 			{...props}
-			className={cn(
-				navLinkDefaultClassName,
-				pathname === props.href && 'bg-accent',
-			)}
+			className={cn(className, pathname === props.href && 'bg-accent')}
 		/>
 	)
 }
