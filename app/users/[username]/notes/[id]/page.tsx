23206bb8f531b16942e2d7380efbd5deaa006802
Alexander Gilbert <alexandergilbert@Alexanders-MacBook-Pro.local>
2023-12-17 19:59:18 +0000

    add meta data

--- app/users/[username]/notes/[id]/page.tsx
+++ app/users/[username]/notes/[id]/page.tsx
@@ -1,10 +1,11 @@
+import type { Metadata } from 'next'
 import Link from 'next/link'
 import { db } from '@/app/lib/db.server'
 import { invariantResponse } from '@/app/lib/misc'
 import { Button } from '@/app/comps/ui/button'
 import { floatingToolbarClassName } from '@/app/comps/floating-toolbar'
 
-type Props = { params: { id: string } }
+type Props = { params: { id: string; username: string } }
 
 export async function loader({ params }: Props) {
 	'use server'
@@ -52,4 +53,20 @@ export default async function NoteRoute(props: Props) {
 			</div>
 		</div>
 	)
+}
+
+export async function generateMetadata(props: Props): Promise<Metadata> {
+	const data = await loader(props)
+
+	const displayName = props.params.username ?? ''
+	const noteTitle = data?.note.title ?? 'Note'
+	const noteContentsSummary =
+		data && data.note.content.length > 100
+			? data?.note.content.slice(0, 97) + '...'
+			: 'No content'
+
+	return {
+		title: `${noteTitle} | ${displayName}'s Notes | Epic Notes`,
+		description: noteContentsSummary,
+	}
 }
