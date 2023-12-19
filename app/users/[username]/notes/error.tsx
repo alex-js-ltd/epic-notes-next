ae13da5370f470cbfb646ccc1f1b8c77bc9f30dc
Alexander Gilbert <alexandergilbert@Alexanders-MacBook-Pro.local>
2023-12-19 15:12:48 +0000

    update errors

--- app/users/[username]/notes/error.tsx
+++
@@ -1,9 +0,0 @@
-'use client' // Error components must be Client Components
-
-import ErrorBoundary from '@/app/comps/error-boundary'
-
-export default function Error(
-	props: React.ComponentProps<typeof ErrorBoundary>,
-) {
-	return <ErrorBoundary {...props} />
-}
