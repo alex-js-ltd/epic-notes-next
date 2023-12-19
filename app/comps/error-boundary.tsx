8947703281b5d9989fdd4f7265e9ec4080a316e1
Alexander Gilbert <alexandergilbert@Alexanders-MacBook-Pro.local>
2023-12-19 14:44:17 +0000

    update

--- app/comps/error-boundary.tsx
+++ app/comps/error-boundary.tsx
@@ -1,7 +1,5 @@
 'use client' // Error components must be Client Components
 
-import { useEffect } from 'react'
-
 export default function ErrorBoundary({
 	error,
 	reset,
@@ -9,12 +7,6 @@ export default function ErrorBoundary({
 	error: Error & { digest?: string }
 	reset: () => void
 }) {
-	useEffect(() => {
-		// Log the error to an error reporting service
-
-		console.log(error.message)
-	}, [error])
-
 	return (
 		<div className="container mx-auto flex h-full w-full items-center justify-center bg-destructive p-20 text-h2 text-destructive-foreground">
 			<p>{error.message}</p>
