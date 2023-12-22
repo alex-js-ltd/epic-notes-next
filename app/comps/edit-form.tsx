84c81ac6e6f668f945af385af5372559949a30c9
Alexander Gilbert <alexandergilbert@Alexanders-MacBook-Pro.local>
2023-12-22 12:22:29 +0000

    update the erros

--- app/comps/edit-form.tsx
+++ app/comps/edit-form.tsx
@@ -22,8 +22,18 @@ export default function EditForm({
 }) {
 	const [state, formAction] = useFormState(editNote, null)
 
+	const formId = 'note-editor'
+
+	const formErrors = [
+		...(state?.errors.title ?? []),
+		...(state?.errors.content ?? []),
+		...(state?.errors.username ?? []),
+		...(state?.errors.id ?? []),
+	]
+
 	return (
 		<form
+			id={formId}
 			method="POST"
 			className="flex h-full flex-col gap-y-4 overflow-x-hidden px-10 pb-28 pt-12"
 			action={formAction}
@@ -33,6 +43,10 @@ export default function EditForm({
 					{/* 🦉 NOTE: this is not an accessible label, we'll get to that in the accessibility exercises */}
 					<Label>Title</Label>
 					<Input name="title" defaultValue={title} required maxLength={100} />
+
+					<div className="min-h-[32px] px-4 pb-3 pt-1">
+						<ErrorList errors={state?.errors?.title} />
+					</div>
 				</div>
 				<div>
 					{/* 🦉 NOTE: this is not an accessible label, we'll get to that in the accessibility exercises */}
@@ -43,6 +57,9 @@ export default function EditForm({
 						required
 						maxLength={10000}
 					/>
+					<div className="min-h-[32px] px-4 pb-3 pt-1">
+						<ErrorList errors={state?.errors.content} />
+					</div>
 				</div>
 			</div>
 			<div className={floatingToolbarClassName}>
@@ -54,8 +71,22 @@ export default function EditForm({
 				</StatusButton>
 			</div>
 
+			<ErrorList errors={formErrors} />
+
 			<input type="hidden" name="id" value={noteId} />
 			<input type="hidden" name="username" value={username} />
 		</form>
 	)
+}
+
+function ErrorList({ errors }: { errors?: Array<string> | null }) {
+	return errors?.length ? (
+		<ul className="flex flex-col gap-1">
+			{errors.map((error, i) => (
+				<li key={i} className="text-[10px] text-foreground-destructive">
+					{error}
+				</li>
+			))}
+		</ul>
+	) : null
 }
