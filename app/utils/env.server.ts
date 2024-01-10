98f94a61f5237a68d49883ab4a68f17f7c8fc363
Alexander Gilbert <alexandergilbert@Alexanders-MacBook-Pro.local>
2024-01-10 17:21:46 +0100

    update

--- app/utils/env.server.ts
+++ app/utils/env.server.ts
@@ -25,9 +25,6 @@ export function init() {
 }
 
 /**
- * This is used in both `entry.server.ts` and `root.tsx` to ensure that
- * the environment variables are set and globally available before the app is
- * started.
  *
  * NOTE: Do *not* add any environment variables in here that you do not wish to
  * be included in the client.
