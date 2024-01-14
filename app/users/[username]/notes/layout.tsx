ee9f0e08cb9dd4934c4a6f6c19161955a575db99
alex-js-ltd <alex.js.ltd@gmail.com>
2024-01-14 01:50:24 +0100

    update the svgs

--- app/users/[username]/notes/layout.tsx
+++ app/users/[username]/notes/layout.tsx
@@ -4,6 +4,7 @@ import ActiveLink from '@/app/comps/ui/active-link'
 import { loadOwner } from '@/app/utils/actions'
 import { auth } from '@clerk/nextjs'
 import { cn } from '@/app/utils/misc'
+import { Icon } from '@/app/comps/ui/icon'
 
 export default async function NotesLayout({
 	params: { username },
@@ -35,20 +36,7 @@ export default async function NotesLayout({
 										href={`/users/${data.owner.username}/notes/new`}
 										className={cn(navLinkDefaultClassName, 'flex items-center')}
 									>
-										<svg
-											className="w-6 h-6 text-currentColor"
-											viewBox="0 0 15 15"
-											fill="none"
-											id="plus"
-										>
-											<path
-												fill-rule="evenodd"
-												clip-rule="evenodd"
-												d="M8 2.75C8 2.47386 7.77614 2.25 7.5 2.25C7.22386 2.25 7 2.47386 7 2.75V7H2.75C2.47386 7 2.25 7.22386 2.25 7.5C2.25 7.77614 2.47386 8 2.75 8H7V12.25C7 12.5261 7.22386 12.75 7.5 12.75C7.77614 12.75 8 12.5261 8 12.25V8H12.25C12.5261 8 12.75 7.77614 12.75 7.5C12.75 7.22386 12.5261 7 12.25 7H8V2.75Z"
-												fill="currentColor"
-											></path>
-										</svg>
-										New Note
+										<Icon name="plus">New Note</Icon>
 									</ActiveLink>
 								</li>
 							) : null}
